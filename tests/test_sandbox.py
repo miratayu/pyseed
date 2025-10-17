22fbd5b09a69931fe3e38779e519857dd14dc2e8
miratayu <54067490+miratayu@users.noreply.github.com>
2025-10-17 14:23:48 +0900

    add assert

--- tests/test_sandbox.py
+++ tests/test_sandbox.py
@@ -3,3 +3,7 @@ import sandbox
 
 def test_increment():
     assert sandbox.increment() == 1
+    assert sandbox.increment(-1) == 0
+    assert sandbox.increment(0) == 1
+    assert sandbox.increment(1) == 2
+    assert sandbox.increment(100) == 101
